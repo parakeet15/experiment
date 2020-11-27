8a4ecc0f98da91295b80a9e64dbbd4417507af7a
parakeet15 <kaisei_19n1102563@nnn.ed.jp>
2020-11-28 00:07:26 +0900

    バグの修正

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // モジュール
-import { removeAllChildren, Diary } from './modules/module.js';
+// import { removeAllChildren, Diary } from './modules/module.js';
 
 // ツールバー
 const createButton = document.getElementById('create-button');
@@ -17,6 +17,16 @@ const saveList = document.getElementById('save-list');
 
 // ドラッグ禁止
 document.ondragstart = () => false;
+
+/**
+ * 指定した要素の子要素を全削除する
+ * @param {HTMLElement} element 
+ */
+function removeAllChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
 
 /**
  * リストアイテムのスタイルを設定する
@@ -49,6 +59,25 @@ addFileInput.addEventListener('change', event => {
         }
     }
 }, false);
+
+/**
+ * 日記
+ */
+class Diary {
+    /**
+     * 内容
+     * @param {string} title 題名
+     * @param {string} content 本文
+     * @param {string} createdAt 作成日時
+     * @param {string} updatedAt 更新日時
+     */
+    constructor(title, content, createdAt, updatedAt) {
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
 
 /**
  * ローカルストレージに記事を保存する
